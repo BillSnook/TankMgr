355deee6d163eaff60370169541d077d313ab3ac
Bill Snook <bluegoose1@mac.com>
2019-01-04 09:13:55 -0800

    Initial Commit

---
+++ TankMgr/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  TankMgr
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project TankMgr
+  Created by William Snook on 1/4/19
+  Copyright © 2019 billsnook
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2018
+  All rights reserved
+  http://embedXcode.weebly.com
+
---
+++ TankMgr/TankMgr.ino
@@ -0,0 +1,67 @@
+//
+// TankMgr
+//
+// Manage power for tank project
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		William Snook
+// 				billsnook
+//
+// Date			1/4/19 9:13 AM
+// Version		<#version#>
+//
+// Copyright	© William Snook, 2019
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+// !!! Help: http://bit.ly/2AdU7cu
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+// !!! Help http://bit.ly/2CL22Qp
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+// !!! Help: http://bit.ly/2l0ZhTa
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
